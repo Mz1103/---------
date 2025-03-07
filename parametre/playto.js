3e607c092ca75f35e426454b0399a984a1532e04
Mz1103 <mzdbot1@gmail.com>
2025-03-07 13:22:48 +0100

    Update playto.js

--- parametre/playto.js
+++ parametre/playto.js
@@ -24,7 +24,7 @@ module.exports = {
           activities: [{
             name: `${str_content}`,
             type: "PLAYING",
-            url: "https://discord.gg/A5bfyv3AzB"
+            url: "https://discord.gg/tHDqSRvSDQ"
           }],
           status: "online"
         });
