7996f1e3f9243d98f87fde20502ade3de36b339a
Mz1103 <mzdbot1@gmail.com>
2025-03-11 22:48:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1316942620613939230', // Buyer
+        buyer: '1349132815198982144', // Buyer
         couleur: '#6495ED', // Couleur HEX
         footer: 'MzProtect', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
