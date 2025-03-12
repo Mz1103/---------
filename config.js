d0129806cd2c4d99e1b3ef9a5f16ed69b4997d13
Mz1103 <mzdbot1@gmail.com>
2025-03-12 23:49:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1349132815198982144', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: 'MzProtect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'discord.gg/luxusshop', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
