28e7dd3bc8b2cebb47d42838b21601b904a71999
Mz1103 <mzdbot1@gmail.com>
2025-03-12 23:50:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1349132815198982144', // Buyer
-        couleur: '#6495ED', // Couleur HEX
+        couleur: '#000000', // Couleur HEX
         footer: 'discord.gg/luxusshop', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
