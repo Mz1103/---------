54d7d6cd5363c682115b2cddc96d09220e9aad4e
Mz1103 <mzdbot1@gmail.com>
2025-03-15 04:10:30 +0100

    Update watch.js

--- parametre/watch.js
+++ parametre/watch.js
@@ -23,7 +23,7 @@ module.exports = {
                     activities: [{
                         name: `${str_content}`,
                         type: "WATCHING",
-                        url: "https://discord.gg/tHDqSRvSDQ"
+                        url: "https://discord.gg/luxusshop"
                     }],
                     status: "online"
                 });
